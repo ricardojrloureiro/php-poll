9670075d7c126ad44f0eebf48cea01d196260be8
Rui Gomes <ruigomeseu@gmail.com>
2014-11-17 19:28:04 +0000

    New style and DB schema

---
+++ db.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users
+(
+	user_id INTEGER PRIMARY KEY,
+	username TEXT,
+	password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS poll
+(
+	poll_id INTEGER PRIMARY KEY,
+	user_id INTEGER,
+	title TEXT,
+	image TEXT,
+	public BOOLEAN,
+
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS options
+(
+	option_id INTEGER PRIMARY KEY,
+	poll_id INTEGER,
+	value TEXT,
+
+	FOREIGN KEY (poll_id) REFERENCES poll(poll_id)
+);
+
+CREATE TABLE IF NOT EXISTS answers
+(
+	answers_id INTEGER PRIMARY KEY,
+	user_id INTEGER,
+	option_id INTEGER,
+
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+	FOREIGN KEY (option_id) REFERENCES options(option_id)
+);
