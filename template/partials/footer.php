73fc22da8c60cad8452758e12f8528f5e6134ee6
Rui Gomes <ruigomeseu@gmail.com>
2014-12-05 16:25:04 +0000

    Partially implemented suggestions

--- template/partials/footer.php
+++ template/partials/footer.php
@@ -3,6 +3,8 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/bloodhound.min.js"></script>
+<script src="js/typeahead.jquery.min.js"></script>
 <script src="js/script.js"></script>
 <script type="text/javascript">
     window.twttr=(function(d,s,id){var t,js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);return window.twttr||(t={_e:[],ready:function(f){t._e.push(f)}})}(document,"script","twitter-wjs"));
